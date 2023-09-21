9d76d87888cb30c473bcb76cc1daafbb5c2e0c81
rbcbrito <robertobcb@gmail.com>
2023-09-21 19:33:22 -0300

    Exercício 1, Aula 151, Seção 13

---
+++ src/com/udemy/nelio/secao13/aula151/Exerc1/Comment.java
@@ -0,0 +1,19 @@
+package com.udemy.nelio.secao13.aula151.Exerc1;
+
+public class Comment {
+
+	private String text;
+
+	public Comment(String text) {
+		this.text = text;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+	
+}
